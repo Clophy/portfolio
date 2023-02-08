3cae3b9d67e99cba1c2d0af229defe34de42313b
Clophy <barangenez@hotmail.com>
2023-02-08 23:30:26 +0300

    Create head.js

---
+++ app/head.js
@@ -0,0 +1,25 @@
+export default function Head(){
+    return(
+        <>
+        <meta charSet="utf-8" />
+        <meta name="robots" content="follow, index" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+  
+        <title>Baran Genez</title>
+        <meta name="description" content="Front-End Developer" />
+        <link rel="canonical" href="https://www.barangenez.com" />
+  
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Baran Genez" />
+        <meta property="og:description" content="Front-End Developer" />
+        <meta property="og:url" content="https://www.barangenez.com" />
+  
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@barangenez" />
+        <meta name="twitter:title" content="Baran Genez" />
+        <meta name="twitter:description" content="Front-End Developer" />
+        <meta name="twitter:url" content="https://www.barangenez.com" />
+      </>
+    )
+}
