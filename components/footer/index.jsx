63104f7e5d8a90aacac4f61618e37611c5fd81d9
Clophy <barangenez@hotmail.com>
2023-02-09 05:05:53 +0300

    Update index.jsx

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -16,21 +16,21 @@ function Footer() {
           <div className={Styles.icon}>
             <FaPhoneSquareAlt />
             <div className={Styles.iconText}>
-              <h6>Phone</h6>
+              <h3>Phone</h3>
               <p>+905456590165</p>
             </div>
           </div>
           <div className={Styles.icon}>
             <AiOutlineMail />
             <div className={Styles.iconText}>
-              <h6>E-mail</h6>
+              <h3>E-mail</h3>
               <p><Link href='mailto:barangenez@hotmail.com'>barangenez@hotmail.com</Link></p>
             </div>
           </div>
           <div className={Styles.icon}>
             <AiFillPlusCircle />
             <div className={Styles.iconText}>
-              <h6>Follow Me</h6>
+              <h3>Follow Me</h3>
               <p><Link href="https://www.twitter.com/barangenez"><FaTwitter/></Link></p>
             </div>
           </div>
