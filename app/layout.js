68c5fb2f48778a3b9f12378596f7ec3726c40894
Clophy <88716639+Clophy@users.noreply.github.com>
2023-04-03 16:24:01 +0300

    a

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import '@/styles/global.css'
 import {Noticia_Text} from '@next/font/google'
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import {Analytics} from '@vercel/analytics/react'
 
 const notic = Noticia_Text({subsets:["latin"], weight:["400","700"]})
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
       <Header active={active} setActive={setActive}/>
       <main>
       {children}
+      <Analytics/>
       </main>
       <Footer/>
       </body>
